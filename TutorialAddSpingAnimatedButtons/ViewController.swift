c4f3f26a30dcda94e4a44f1c829b41e7f0a6ec9d
Eirik Madland <eirik@madland.info>
2015-08-31 21:49:53 +0200

    Initial Commit

---
+++ TutorialAddSpingAnimatedButtons/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TutorialAddSpingAnimatedButtons
+//
+//  Created by Eirik Madland on 31.08.15.
+//  Copyright (c) 2015 DevApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
